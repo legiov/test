f4456fd3a30abff7d707e7a169397235b2fcc963
Вадим <@LegionV>
2014-11-04 17:04:24 +0400

    tests fix

--- src/Blog/AdminBundle/Tests/Controller/SecurityControllerTest.php
+++ src/Blog/AdminBundle/Tests/Controller/SecurityControllerTest.php
@@ -22,6 +22,57 @@ class SecurityControllerTest extends WebTestCase
         $this->assertCount(1, $crawler->filter('input#username'), 'no username input found');
         $this->assertCount(1, $crawler->filter('input#password'), 'no password input found');
         $this->assertCount(1, $crawler->filter('input[type=submit]'), 'no submit button found');
+        
+        $form = $crawler->selectButton('_submit')->form(array(
+            '_username' => 'admin',
+            '_password' => 'adminpass'
+        ));
+        
+        $client->submit($form);
+        
+        $this->assertTrue( $client->getResponse()->isRedirect(),'There was no redirection after authentication' );
+        
+        $crawler = $client->followRedirect();
+        
+        $link = $crawler->filter('a:contains("Exit")')->first()->link();
+        
+        $client->click($link);
+        
+        $this->assertTrue( $client->getResponse()->isRedirect(),'There was no redirection after logout');
+        
+    }
+    
+    public function testLoginRu()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/ru/login');
+        
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'Response is not successfull');
+        
+        $this->assertEquals('Панель администратора', $crawler->filter('header > p')->text(), 'Title is wrong');
+        
+        $this->assertCount(1, $crawler->filter('input#username'), 'no username input found');
+        $this->assertCount(1, $crawler->filter('input#password'), 'no password input found');
+        $this->assertCount(1, $crawler->filter('input[type=submit]'), 'no submit button found');
+        
+        $form = $crawler->selectButton('_submit')->form(array(
+            '_username' => 'admin',
+            '_password' => 'adminpass'
+        ));
+        
+        $client->submit($form);
+        
+        $this->assertTrue( $client->getResponse()->isRedirect(),'There was no redirection after authentication' );
+        
+        $crawler = $client->followRedirect();
+        
+        $link = $crawler->filter('a:contains("Выход")')->first()->link();
+        
+        $client->click($link);
+        
+        $this->assertTrue( $client->getResponse()->isRedirect(),'There was no redirection after logout');
+        
     }
 
 
